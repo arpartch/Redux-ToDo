458cf957a3f7e7f012c70932963ded00e5364455
arpartch <athena.partch@gmail.com>
2021-04-24 10:22:57 -0700

    Get Todos from the API

--- src/redux/todoSlice.js
+++ src/redux/todoSlice.js
@@ -1,8 +1,18 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+
+export const getTodosAsync = createAsyncThunk(
+	'todos/getTodosAsync',
+	async () => {
+		const resp = await fetch('http://localhost:7000/todos');
+		if (resp.ok) {
+			const todos = await resp.json();
+			return { todos };
+		}
+	}
+);
 
 export const todoSlice = createSlice({
 	name: 'todos',
-  /*Create inital state */
 	initialState: [
 		{ id: 1, title: 'todo1', completed: false },
 		{ id: 2, title: 'todo2', completed: false },
@@ -10,7 +20,6 @@ export const todoSlice = createSlice({
 		{ id: 4, title: 'todo4', completed: false },
 		{ id: 5, title: 'todo5', completed: false },
 	],
-  /* Reducer - applying dispatched actions type, payload */
 	reducers: {
 		addTodo: (state, action) => {
 			const todo = {
@@ -18,22 +27,23 @@ export const todoSlice = createSlice({
 				title: action.payload.title,
 				completed: false,
 			};
-      /* pust change state to store */
 			state.push(todo);
 		},
-    toggleComplete: (state, action) => {
+		toggleComplete: (state, action) => {
 			const index = state.findIndex((todo) => todo.id === action.payload.id);
 			state[index].completed = action.payload.completed;
-    },
-    deleteTodo: (state, action) => {
+		},
+		deleteTodo: (state, action) => {
 			return state.filter((todo) => todo.id !== action.payload.id);
-    },
+		},
+	},
+    	extraReducers: {
+		[getTodosAsync.fulfilled]: (state, action) => {
+			return action.payload.todos;
+		},
 	},
 });
 
-/*making the state and actions avialable to other components */
-
-/*dispactched */
 export const { addTodo, toggleComplete, deleteTodo } = todoSlice.actions;
 
 export default todoSlice.reducer;
