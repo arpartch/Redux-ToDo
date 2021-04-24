8eed5236f049fe3a7f96ec85a1b2416f6dbcc305
arpartch <athena.partch@gmail.com>
2021-04-24 09:00:18 -0700

    add comments

--- src/redux/todoSlice.js
+++ src/redux/todoSlice.js
@@ -2,6 +2,7 @@ import { createSlice } from '@reduxjs/toolkit';
 
 export const todoSlice = createSlice({
 	name: 'todos',
+  /*Create inital state */
 	initialState: [
 		{ id: 1, title: 'todo1', completed: false },
 		{ id: 2, title: 'todo2', completed: false },
@@ -9,6 +10,7 @@ export const todoSlice = createSlice({
 		{ id: 4, title: 'todo4', completed: false },
 		{ id: 5, title: 'todo5', completed: false },
 	],
+  /* Reducer - applying dispatched actions type, payload */
 	reducers: {
 		addTodo: (state, action) => {
 			const todo = {
@@ -16,13 +18,16 @@ export const todoSlice = createSlice({
 				title: action.payload.title,
 				completed: false,
 			};
+      /* pust change state to store */
 			state.push(todo);
 		},
 
 	},
 });
 
+/*making the state and actions avialable to other components */
 
+/*dispactched */
 export const { addTodo } = todoSlice.actions;
 
 export default todoSlice.reducer;
